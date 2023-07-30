8f245cde7dcbb4f1df19766ace9e3b68e84dae8f
gustavofranco88 <gustavofranco88@gmail.com>
2023-07-29 23:35:19 -0300

    CRIANDO A PRIMEIRA TABELA DO APP_AGENDA

--- app_agenda/models.py
+++ app_agenda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data = models.DateTimeField(verbose_name='Data')
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Criado em')
+
+    class Meta:
+        ''' aqui estou forçando o nome da tabela como 'evento' '''
+        db_table = 'evento'
