1d0a23e464449030e1f0adb1255b2478d140503f
gustavofranco88 <gustavofranco88@gmail.com>
2023-07-30 13:45:35 -0300

    Finalizando a tabela Eventos e adicinando filtros de visualização

--- app_agenda/admin.py
+++ app_agenda/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+#importando todas as classes do aplicativo
+from app_agenda.models import *
+
+#tratamento para visualização das classes na pagina em questão
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data', 'data_criacao')
+    list_filter = ('titulo', 'descricao', 'data', 'data_criacao', 'usuario')
+
 # Register your models here.
+admin.site.register(Evento, EventoAdmin)
--- app_agenda/models.py
+++ app_agenda/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import * #importando classes do django admin
 
 # Create your models here.
-
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data = models.DateTimeField(verbose_name='Data')
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='Criado em')
+    #criando uma foreignkey da classe django-admin
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    #on_delete=models.CASCADE é obrigatorio, caso um usuario seja deletado, todos os eventos dele serão tbm
+    
 
     class Meta:
         ''' aqui estou forçando o nome da tabela como 'evento' '''
         db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
