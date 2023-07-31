cc10e729320f5b61fb5fde527de7236dd80a3f1d
gustavofranco88 <gustavofranco88@gmail.com>
2023-07-31 07:08:24 -0300

    Atualizando a view e o html

--- app_agenda/views.py
+++ app_agenda/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from app_agenda.models import Evento
 # Create your views here.
 
 def lista_eventos(request):
-    render(request, 'agenda.html')
+    evento = Evento.objects.all()
+    response = {'eventos': evento}
+    return render(request, 'agenda.html', response)
 
